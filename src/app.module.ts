42eac25e3ffd0126d09431274a22dd8b0d1055ea
bujosa <davidbujosa@gmail.com>
2023-06-08 02:47:56 -0400

    feat: init user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DevtoolsModule } from '@nestjs/devtools-integration';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     DevtoolsModule.register({
       http: process.env.NODE_ENV !== 'production',
     }),
+    UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
